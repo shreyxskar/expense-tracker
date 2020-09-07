79d1e0a6b9c75eae7f4e2fb78bb7110007d913eb
shreyxskar <shreyaskar.srivastava@outlook.com>
2020-09-07 21:46:31 +0530

    Added user login feature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 import pymongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
@@ -6,22 +6,67 @@ import datetime
 
 app = Flask(__name__)
 
-app.secret_key = 'myExpenseApp'
+app.secret_key = 'myExpenseApp#with#a#veryLongSecretKey#thanksForReadingThisLine#loveYou'
 
 connection = MongoClient('localhost', 27017)
-database = connection['user_db01']
+database = connection['extr_db']
 
 cat_types = ['Bills', 'Bonus', 'Entertainment', 'Food', 'Health', 'House', 'Salary', 'Transport', 'Extras']
 
+
+@app.route('/signup', methods=['POST'])
+def signup_user():
+
+    global database
+
+    username = request.form['username']
+    password = request.form['password']
+    fname = request.form['firstname']
+
+    user_data = database['users']
+    if not user_data.find_one({'username': username}):
+        user_data.insert_one({'username': username, 'password': password, 'name': fname})
+        flash('User added succesfully!')
+    else:
+        flash('Username already exists!')
+
+    return redirect(url_for('index'))
+
+
+@app.route('/signin', methods=['POST'])
+def signin_user():
+
+    global database
+
+    username = request.form['username']
+    password = request.form['password']
+
+    if database['users'].find_one({'username': username, 'password': password}):
+        session.pop('user', None)
+        session['user'] = username
+        return redirect(url_for('index_page'))
+    
+    flash('Invalid username/password!')
+    return redirect(url_for('index'))    
+
+
 @app.route('/', methods=['GET'])
 def index():
-
-    #return '<p>TODO</p><a href=\"' + url_for('index_page') + '\">Click here </a>'
+    
     return render_template('login.html')
 
 
+@app.route('/signout_user', methods=['POST', 'GET'])
+def signout_user():
+
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+
 @app.route('/dashboard', methods=['GET', 'POST'])
-def index_page():
+def index_page():    
+
+    if 'user' not in session: return redirect(url_for('index'))
     
     global database
     collection = database['expenses']
@@ -36,7 +81,7 @@ def index_page():
     upper_bound = str(datetime.date(year, month+1, 1))
     lower_bound = str(datetime.date(year, month, 1))
 
-    res = collection.find({'$and': [{'date': {'$lt': upper_bound}}, {'date': {'$gte': lower_bound}}]})
+    res = collection.find({'$and': [{'date': {'$lt': upper_bound}}, {'date': {'$gte': lower_bound}}, {'username': session['user']}]})
 
     month_list, year_list = [], []
     for i in range(12):
@@ -61,33 +106,39 @@ def index_page():
             records['debit_sum'] += float(i['transaction_amount'])
         else: #credit
             records['credit_sum'] += float(i['transaction_amount'])
+
+    fname = database['users'].find_one({'username': session['user']})
     
-    return render_template('index_page.html', monthly_records=records, months=month_list, years=year_list)
+    return render_template('index_page.html', monthly_records=records, months=month_list, years=year_list, user=fname['name'])
 
 
 @app.route('/manage_accounts', methods=['GET', 'POST'])
 def manage_accounts():
 
+    if 'user' not in session: return redirect(url_for('index'))
+
     global database
     collection = database['accounts']
     if request.method == 'POST':
         acc_name = request.form['acc_name']
-        collection.insert_one({'account_name': acc_name, 'date_created': datetime.datetime.now()})
+        collection.insert_one({'account_name': acc_name, 'date_created': datetime.datetime.now(), 'username': session['user']})
         flash('Account added!')
         return redirect(url_for('manage_accounts'))
 
-    accounts = collection.find()
+    accounts = collection.find({'username': session['user']})
     return render_template('manage_accounts.html', X=accounts)
 
 
 @app.route('/add_expense', methods=['GET', 'POST'])
 def input_form():
 
+    if 'user' not in session: return redirect(url_for('index'))
+
     global cat_types
     global database
 
     if request.method == "GET":              
-        return render_template('input_form.html', cats=cat_types, accounts=database['accounts'].find().distinct('account_name'))
+        return render_template('input_form.html', cats=cat_types, accounts=database['accounts'].find({'username': session['user']}).distinct('account_name'))
            
     data = {}
     data['transaction_amount'] = request.form['eamount']
@@ -104,40 +155,51 @@ def input_form():
     data['category'] = request.form['ecategory'] 
     data['account'] = request.form['eaccount']
 
+    data['username'] = session['user']
+
     collection = database['expenses']
     collection.insert_one(data)
     flash('Expense added!')
     
-    return render_template('input_form.html', cats=cat_types, accounts=database['accounts'].find().distinct('account_name'))
+    return render_template('input_form.html', cats=cat_types, accounts=database['accounts'].find({'username': session['user']}).distinct('account_name'))
 
 
 @app.route('/all_expenses', methods=['GET'])
 def all_expenses():
 
+    if 'user' not in session: return redirect(url_for('index'))
+
     global database
     collection = database['expenses']    
-    return render_template('all_expenses.html', docs_c=collection.find({}).sort('date', -1), accounts=database['accounts'].find().distinct('account_name'))
+    return render_template('all_expenses.html', docs_c=collection.find({'username': session['user']}).sort('date', -1), accounts=database['accounts'].find({'username': session['user']}).distinct('account_name'))
 
 
 @app.route('/all_expenses/<account>')
 def all_expenses_filter(account):
 
+    if 'user' not in session: return redirect(url_for('index'))
+
     global database
     collection = database['expenses']    
-    return render_template('all_expenses.html', docs_c=collection.find({'account': account}).sort('date', -1), accounts=database['accounts'].find().distinct('account_name'))
+    return render_template('all_expenses.html', docs_c=collection.find({'account': account, 'username': session['user']}).sort('date', -1), accounts=database['accounts'].find({'username': session['user']}).distinct('account_name'))
 
 
 @app.route('/manage_expenses', methods=['GET', 'POST'])
-def manage_expenses():  
+def manage_expenses():
+
+    if 'user' not in session: return redirect(url_for('index'))
+
     global database 
     global cat_types
 
     collection = database['expenses']
-    return render_template('manage_expenses.html', docs_c=collection.find({}), cats=cat_types)
+    return render_template('manage_expenses.html', docs_c=collection.find({'username': session['user']}), cats=cat_types)
 
 
 @app.route('/delete_expense/<expense_id>', methods=['POST', 'GET'])
-def delete_expense(expense_id):    
+def delete_expense(expense_id):   
+
+    if 'user' not in session: return redirect(url_for('index')) 
 
     global database
     collection = database['expenses']
@@ -149,6 +211,9 @@ def delete_expense(expense_id):
 
 @app.route('/update_expense', methods=['POST', 'GET'])
 def update_expense():
+
+    if 'user' not in session: return redirect(url_for('index'))
+
     if request.method == 'GET':
         return redirect(url_for('index_page'))
     
